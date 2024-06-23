79faead8e267d0a4c76b7fcf99393aa0b12e2b4f
Ro-Rohit <Ro-Rohit@users.noreply.github.com>
2024-06-23 05:22:50 +0000

    deploy: eaf809bba528ac9c10816b432a5838bbe9962e92

---
+++ _next/static/chunks/pages/_error-f60551ad02476d47.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{2904:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return c(6135)}])},6135:function(a,b,c){"use strict";c.r(b);var d=c(5893),e=function(){return(0,d.jsx)("div",{children:"Error Page"})};b.default=e}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=2904)}),_N_E=a.O()}])
